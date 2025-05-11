587e6a6b4037719909b3a51386ec223386dca4b6
Mathieu Bridon <mathieu@daitauha.fr>
2025-05-11 10:57:28 +0200

    Properly hadle the DB in Railway

--- resume/settings.py
+++ resume/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,10 +75,9 @@ WSGI_APPLICATION = "resume.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL", ""), conn_max_age=600
+    )
 }
 
 
